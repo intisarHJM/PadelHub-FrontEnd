6ced6f1320aca94ad0c78a8e89b90ef224f575f4
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-04-19 13:32:01 +0300

    sign-up form is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css"
+import React from "react"
+import SignUp from "./pages/SignUp"
+import { useState } from "react"
+import { Route, Routes } from "react-router"
+import Welcome from "./pages/Welcome"
 
 const App = () => {
-
   return (
-    <>
-
-    </>
+    <main>
+      <Routes>
+         <Route path="/" element={<Welcome />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </main>
   )
 }
 
-export default App 
+export default App
