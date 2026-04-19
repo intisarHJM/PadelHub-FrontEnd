d744521d9d78f32607d706360896160bf0e9b37a
Rehab <rehabmohd03@gmail.com>
2026-04-19 10:58:26 +0300

    add About Page

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Page</h1>
+      <p>
+      Padel Club Raed provides professional courts (indoor and outdoor) with the highest specifications. We make it easy for you to book, choose equipment, and enjoy the best padel facilities in Bahrain.
+      </p>
+    </div>
+  )
+}
+
+export default About
